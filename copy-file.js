eb0460aa361794cc47da2b47441b0e2aa086b25d
darthnihilus0917 <jcsantosdevtest@gmail.com>
2024-05-10 09:19:34 +0800

    Initial commit

---
+++ copy-file.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const fsFolders = require('fs-extra');
+const fs = require('fs');
+
+const destinationBuildFolder = "./deploy/tmm";
+
+const folderList = [
+    'meat_output',
+    'output',
+    'raw_data',
+    'templates',
+    'monitoring'
+]
+
+const files = [
+    'logs.txt',
+    '.env'
+]
+
+const deployFiles = () => {
+    // COPY FILE
+    files.forEach((file) => {
+        const fileToBeCopied = path.basename(file);    
+        const destination = path.join(destinationBuildFolder, fileToBeCopied);
+        fs.copyFileSync(file, destination);
+    });
+
+    // CREATE FOLDER
+    folderList.forEach((folder) => {
+        const folderPath = path.basename(folder)
+        const newFolderPath = path.join(destinationBuildFolder, folderPath);
+        
+        fs.mkdir(newFolderPath, { recursive: true }, (err) => {
+            if (err) {
+                console.error(`Error creating folder: ${err}`);
+                process.exit(0);
+            }      
+        });
+
+        // COPY SUBFOLDER CONTENT
+        const sourceFolder = `./${folder}`;
+        fsFolders.copy(sourceFolder, `${destinationBuildFolder}/${folder}`, (err) => {
+            if (err) {
+                console.error(`Error copying files: ${err}`);
+                process.exit(0);
+            }  
+        });
+    });
+}
+
+try {
+    if (fs.existsSync(destinationBuildFolder)) {
+        deployFiles();
+    } else {
+        // CREATE BUILD DIRECTORY
+        fs.mkdirSync(destinationBuildFolder);
+        // CREATE FOLDERS AND COPY FILES
+        deployFiles();
+    }
+
+} catch (err) {
+    console.error(`Error checking BUILD folder existence: ${err}`);
+}
